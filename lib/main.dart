f51b9ada7f4e8b8638993250b8b760eebe5213c0
Paul C <pcutter@cnm.edu>
2021-01-23 20:37:19 -0700

    changed to dark theme, removed dbug banner

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       home: InputPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
